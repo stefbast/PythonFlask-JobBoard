50848b0ad3e3127fc33ac700990b595bb4c5728e
Stefaan Bastin <stefaan.bastin@tui.be>
2020-02-20 10:54:12 +0100

    Display Individual Jobs

--- jobs/app.py
+++ jobs/app.py
@@ -24,14 +24,23 @@ def execute_sql(sql, values=(), commit=False, single=False):
     cursor.close()
     return results
 
+
+@app.route('/job/<job_id>')
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    return render_template('job.html', job=job)
+
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if not connection is None:
         connection.close()
 
+
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    jobs = execute_sql("SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id")
+    jobs = execute_sql(
+        "SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id")
     return render_template('index.html', jobs=jobs)
